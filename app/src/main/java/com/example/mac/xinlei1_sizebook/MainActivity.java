0a2a726bb6d44c7a9f1acc4fbdb51b1cef331e6a
Raymundo1 <cquptcxl@gmail.com>
2017-01-23 22:16:05 -0700

    Add menu

--- app/src/main/java/com/example/mac/xinlei1_sizebook/MainActivity.java
+++ app/src/main/java/com/example/mac/xinlei1_sizebook/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.mac.xinlei1_sizebook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Add menu UI
+     * @param menu
+     * @return true
+     */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.book_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_person:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+        }
+        return true;
     }
 }
